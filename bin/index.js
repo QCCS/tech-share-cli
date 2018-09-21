6484f788b63a965e04775c2c956de75aceca384d
周立 <li.zhou@huilianyi.com>
2018-09-21 10:37:34 +0800

    share3

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
 console.log("init start")
 var fs = require("fs");
-const path = require('path');
 
 let package = require('../package.json');
 const program = require('commander');
 const download = require('download-git-repo');
 // const writePage = require('.....');
 
-
 program.version(package.version, '-v,--version')
     .command('init <name> [branch]')
     .action((name, branch) => {
         console.log(name)
         console.log(branch)
-        console.log("load project")
+        console.log("load project ...")
         if (branch) {
             download('direct:https://github.com/QCCS/tech-share.git#' + branch, name, {clone: true}, function (err) {
                 console.log(err ? 'Init Error' : 'Init Success')
@@ -24,35 +22,41 @@ program.version(package.version, '-v,--version')
                 console.log(err ? 'Init Error' : 'Init Success')
             })
         }
+    });
 
-    })
-    .option('new', 'generate a page')
-    .parse(process.argv);
+program.version(package.version)
+    .command('new <name> [pageName]')
+    .action((name, pageName) => {
+        if(name != "page" || !pageName){
+            console.log("if you want to create page,type--- share-tech-cli new page pageName")
+            return;
+        }
+        writePage(pageName);
+    });
+
+program.parse(process.argv);
 
 
-if (program.new) {
-    // process.argv获取输入内容
-    writePage(process.argv);
-}
-
-
-function writePage(args) {
-    //页面文件夹与页面名字，相同
-    let pageName = args[3];
+//创建目录
+function writePage(pageName) {
+    //获取项目路径
+    let cwd = process.cwd();
     //页面内容
     let txt = "<html>";
-    let path = __dirname + "/src/js/pages/" + pageName;
+    let path = cwd + "/src/js/pages/" + pageName;
     let pathName = path + "/" + pageName + ".html";
     //生成的页面路径
     fs.mkdir(path, function (err) {
         if (err) {
-            console.log('file create error！');
+            console.log(path)
+            console.log('path create error！');
             return false;
         }
-        console.log('file create success！');
+        console.log('path create success！');
         //生成的页面
         fs.writeFile(pathName, txt, function (err) {
             if (err) {
+                console.log(pathName)
                 console.log("generate a page err");
                 return false;
             }
